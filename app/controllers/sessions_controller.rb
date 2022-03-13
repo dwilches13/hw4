e81569bd9c8a71487785814bcb81d891f0f2ec0b
dwilches13 <daniel.wilches@kellogg.northwestern.edu>
2022-03-13 14:25:36 +0000

    2nd Commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,15 @@ class SessionsController < ApplicationController
     entered_email = params["email"]
     entered_password = params["password"]
     @user = User.find_by({email: entered_email})
+    if @user
+      if BCrypt::Password.new(@user.password) == entered_password
+        redirect_to "/places"
+      else 
+        redirect_to "/sessions/new"
+      end
+    else
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
